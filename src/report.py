029e1ebb29538201523bb43d537d3db0c9099f24
Jeremy Traini <z5362885@ad.unsw.edu.au>
2023-03-05 13:44:26 +1100

    Adding stubs and endpoints for peppol and syntax endpoints

---
+++ src/report.py
@@ -0,0 +1,10 @@
+from typing import Dict
+
+
+# Syntax report stub
+def report_syntax_v1() -> Dict:
+    return {}
+
+# Peppol report stub
+def report_peppol_v1() -> Dict:
+    return {}
--- src/server.py
+++ src/server.py
@@ -1,10 +1,12 @@
 import signal
 from src.config import base_url, port
 from src.health_check import health_check_v1
+from src.report import report_syntax_v1, report_peppol_v1
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import JSONResponse
 from src.error import AuthenticationError, InputError
 import uvicorn
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -25,6 +27,22 @@ async def validation_exception_handler(request: Request, exc: Exception):
 @app.get("/health_check/v1")
 async def health_check():
     return health_check_v1()
+
+
+@app.post("/report/syntax/v1")
+async def report_syntax(name: str = "My Invoice",
+                        format: str = "xml",
+                        source: str = "text",
+                        data: str = ""):
+    return report_syntax_v1()
+
+
+@app.post("/report/peppol/v1")
+async def report_peppol(name: str = "My Invoice",
+                        format: str = "xml",
+                        source: str = "text",
+                        data: str = ""):
+    return report_peppol_v1()
 
 
 # Samples below
