c312b1d1cbfd3fe1578150910f35d9a75135cfdb
dzl-i <denzel.iskandar@gmail.com>
2023-03-05 19:23:46 +1100

    Adding helper function to invalidate invoices

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,7 +1,54 @@
+import lxml.etree as ET
 
+def invalidate_invoice(choice, tag_name, text):
+    '''
+    Invalidating the default invoice (AUInvoice.xml) by changing either the tag or the content into a new text.
+    Writes the output into a file tests/output.xml
 
-# Used to invalidate invoice
-# To be replaced with Denzel's better function
-def remove_part_of_string(string, start, end):
-    return string[:start] + string[end:]
+    Arguments:
+        choice (str) - Choice whether the user wants to replace the tag or the content
+        tag_name (str) - The tag that we want to change
+        text (str) - The replacement text for either the tag or the content 
 
+    Exceptions:
+        None
+
+    Return Value:
+        None
+    
+    Sample Calls:
+    invalidate_invoice(
+        'tag', 
+        '{urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2}CustomizationID', 
+        '{urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2}TEST1'
+    )
+
+    invalidate_invoice(
+        'tag', 
+        '{urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2}InvoicePeriod', 
+        '{urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2}TEST2'
+    )
+
+    invalidate_invoice(
+        'content',
+        '{urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2}CustomizationID',
+        'Hello World!'
+    )
+    '''
+
+    #adding the encoding when the file is opened and written is needed to avoid a charmap error
+    with open('tests/AUInvoice.xml', encoding="utf8") as f:
+        tree = ET.parse(f)
+        root = tree.getroot()
+
+        for elem in root.getiterator():
+            try:
+                if choice == 'tag' and elem.tag == tag_name:
+                    elem.tag = text
+                elif choice == 'content' and elem.tag == tag_name:
+                    elem.text = text
+            except AttributeError:
+                pass
+
+    # Adding the xml_declaration and method helped keep the header info at the top of the file.
+    tree.write('tests/InvalidInvoice.xml', xml_declaration=True, method='xml', encoding="utf8")
