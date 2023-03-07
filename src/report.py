bc1750bcee7e6a1dcec28fb173a741421c419c76
Jeremy Traini <z5362885@ad.unsw.edu.au>
2023-03-07 20:29:23 +1100

    Getting saxon to parse xslt for PEPPOL and output errors

--- src/report.py
+++ src/report.py
@@ -1,4 +1,5 @@
 from typing import Dict
+from saxonche import PySaxonProcessor
 
 
 # Syntax report stub
@@ -7,4 +8,24 @@ def report_syntax_v1(name, format, source, data) -> Dict:
 
 # Peppol report stub
 def report_peppol_v1(name, format, source, data) -> Dict:
-    return {}
+    with PySaxonProcessor(license=False) as proc:
+        xsltproc = proc.new_xslt30_processor()
+        executable = xsltproc.compile_stylesheet(stylesheet_file="AUNZ-PEPPOL-validation.xslt")
+        
+        node = proc.parse_xml(xml_text=data)
+        schematron_output = executable.transform_to_value(source_text=node)
+            
+        # schematron_output = executable.transform_to_value(source_file="AUInvoice3.xml")
+        
+        if not schematron_output:
+            print(schematron_output.error_message)
+            print("messed")
+            exit()
+        
+        violations = []
+        is_valid = True
+        rules_failed = set()
+        
+        result = []
+        
+        return result
