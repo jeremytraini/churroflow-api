0e0da97de1876ce29f6d562e73de4fcaa272dc9a
RicardoAzzi <rnazzi1@hotmail.com>
2023-03-07 21:54:50 +1100

    wellformedness tests

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,7 +1,11 @@
-
-
 # Used to invalidate invoice
 # To be replaced with Denzel's better function
 def remove_part_of_string(string, start, end):
     return string[:start] + string[end:]
 
+def replace_part_of_string(string, start, end, replace):
+    return string[:start] + replace + string[end:]
+
+
+def append_to_string(string, txt):
+    return string + txt
--- tests/wellformedness_test.py
+++ tests/wellformedness_test.py
@@ -1,6 +1,6 @@
 from tests.server_calls import report_wellformedness_v1
 from tests.constants import VALID_INVOICE_TEXT
-from tests.helpers import remove_part_of_string
+from tests.helpers import remove_part_of_string, append_to_string, replace_part_of_string
 
 """
 =====================================
@@ -46,6 +46,177 @@ def test_wellformed_valid_invoice():
 
     # Check that the location xpath is not empty
     assert abn_violation["location"]["xpath"]
+
+
+def test_two_root_elements_invalid():
+    name = "My Invoice"
+    format = "xml"
+    source = "text"
+    data = VALID_INVOICE_TEXT
+
+    data = append_to_string(data, """<root><h>Second root at the end</h></root>""")
+    wellformed_evaluation = report_wellformedness_v1(name, format, source, data)
+
+    # We expect exactly 1 rule to fail due to having two root elements
+    assert wellformed_evaluation["num_rules_failed"] == 1
+
+    # We expect exactly 1 violation due to the two root elements
+    assert wellformed_evaluation["num_violations"] == 1
+
+    # Thus there should be exactly 1 violation in the violation list
+    assert len(wellformed_evaluation["violations"]) == 1
+
+    violation = wellformed_evaluation["violations"][0]
+
+    # Check that the violation is for the correct rule and is flagged as fatal
+    assert violation["rule_id"] == "errorName" # need to find correct rule_id
+    assert violation["is_fatal"] == True
+
+    # Check that the violation has a non-empty message, test and suggestion
+    assert violation["message"]
+    assert violation["test"]
+    assert violation["suggestion"]
+
+def test_no_closing_tag_invalid():
+    name = "My Invoice"
+    format = "xml"
+    source = "text"
+    data = VALID_INVOICE_TEXT
+
+    data = remove_part_of_string(data, 11530, 11540)
+    wellformed_evaluation = report_wellformedness_v1(name, format, source, data)
+
+    # We expect exactly 1 rule to fail due to having no closing tag for root element
+    assert wellformed_evaluation["num_rules_failed"] == 1
+
+    # We expect exactly 1 violation due to the missing closing tag
+    assert wellformed_evaluation["num_violations"] == 1
+
+    # Thus there should be exactly 1 violation in the violation list
+    assert len(wellformed_evaluation["violations"]) == 1
+
+    violation = wellformed_evaluation["violations"][0]
+
+    # Check that the violation is for the correct rule and is flagged as fatal
+    assert violation["rule_id"] == "errorName" # need to find correct rule_id
+    assert violation["is_fatal"] == True
+
+    # Check that the violation has a non-empty message, test and suggestion
+    assert violation["message"]
+    assert violation["test"]
+    assert violation["suggestion"]
+
+
+# Tag opens in a nest but closes outside
+def test_wrong_nesting_invalid():
+    name = "My Invoice"
+    format = "xml"
+    source = "text"
+    data = VALID_INVOICE_TEXT
+
+    data = remove_part_of_string(data, 11512, 11530)
+    data = append_to_string(data, """</cac:InvoiceLine>""") 
+    wellformed_evaluation = report_wellformedness_v1(name, format, source, data)
+
+    # We expect exactly 1 rule to fail due to having no closing tag in the corresponding nest
+    assert wellformed_evaluation["num_rules_failed"] == 1
+
+    # We expect exactly 1 violation due to the missing closing tag in the nest
+    assert wellformed_evaluation["num_violations"] == 1
+
+    # Thus there should be exactly 1 violation in the violation list
+    assert len(wellformed_evaluation["violations"]) == 1
+
+    violation = wellformed_evaluation["violations"][0]
+
+    # Check that the violation is for the correct rule and is flagged as fatal
+    assert violation["rule_id"] == "errorName" # need to find correct rule_id
+    assert violation["is_fatal"] == True
+
+    # Check that the violation has a non-empty message, test and suggestion
+    assert violation["message"]
+    assert violation["test"]
+    assert violation["suggestion"]
+
+
+def test_no_escape_for_special_char_invalid():
+    name = "My Invoice"
+    format = "xml"
+    source = "text"
+    data = VALID_INVOICE_TEXT
+
+    data = replace_part_of_string(data, 499, 500, "<")
+    wellformed_evaluation = report_wellformedness_v1(name, format, source, data)
+
+    # We expect exactly 1 rule to fail due to not escaping a special character
+    assert wellformed_evaluation["num_rules_failed"] == 1
+
+    # We expect exactly 1 violation due to the special character
+    assert wellformed_evaluation["num_violations"] == 1
+
+    # Thus there should be exactly 1 violation in the violation list
+    assert len(wellformed_evaluation["violations"]) == 1
+
+    violation = wellformed_evaluation["violations"][0]
+
+    # Check that the violation is for the correct rule and is flagged as fatal
+    assert violation["rule_id"] == "errorName" # need to find correct rule_id
+    assert violation["is_fatal"] == True
+
+    # Check that the violation has a non-empty message, test and suggestion
+    assert violation["message"]
+    assert violation["test"]
+    assert violation["suggestion"]
+
+
+    # Tag opens in a nest but closes outside
+def test_no_escape_for_special_char_invalid():
+    name = "My Invoice"
+    format = "xml"
+    source = "text"
+    data = VALID_INVOICE_TEXT
+
+    data = replace_part_of_string(data, 499, 500, "<")
+    wellformed_evaluation = report_wellformedness_v1(name, format, source, data)
+
+    # We expect exactly 1 rule to fail due to not escaping a special character
+    assert wellformed_evaluation["num_rules_failed"] == 1
+
+    # We expect exactly 1 violation due to the special character
+    assert wellformed_evaluation["num_violations"] == 1
+
+    # Thus there should be exactly 1 violation in the violation list
+    assert len(wellformed_evaluation["violations"]) == 1
+
+    violation = wellformed_evaluation["violations"][0]
+
+    # Check that the violation is for the correct rule and is flagged as fatal
+    assert violation["rule_id"] == "errorName" # need to find correct rule_id
+    assert violation["is_fatal"] == True
+
+    # Check that the violation has a non-empty message, test and suggestion
+    assert violation["message"]
+    assert violation["test"]
+    assert violation["suggestion"]
+
+
+def test_escape_special_char_valid():
+    name = "My Invoice"
+    format = "xml"
+    source = "text"
+    data = VALID_INVOICE_TEXT
+
+    data = replace_part_of_string(data, 499, 503, "&lt;")
+    wellformed_evaluation = report_wellformedness_v1(name, format, source, data)
+
+    # We expect exactly 0 rules to fail since the special character was escaped
+    assert wellformed_evaluation["num_rules_failed"] == 0
+
+    # We expect exactly 0 violations
+    assert wellformed_evaluation["num_violations"] == 0
+
+    # Thus there should've be any violation
+    assert len(wellformed_evaluation["violations"]) == 0
 
 
 # Root element - closing tag + nesting - Ricardo
