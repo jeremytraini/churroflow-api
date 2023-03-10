6e0af731bc2e4b1f28d58ffc1e7d9e00bae356a9
Ahona <z5363845@ad.unsw.edu.au>
2023-03-10 21:08:59 +1100

    Updated wellformedness changes to master version

--- tests/report/wellformedness_test.py
+++ tests/report/wellformedness_test.py
@@ -1,12 +1,10 @@
 from src.types import *
-import pydantic
 from tests.server_calls import report_wellformedness_v1
 from tests.constants import VALID_INVOICE_TEXT
-from tests.helpers import remove_part_of_string, append_to_string, replace_part_of_string, invalidate_invoice
-
+from tests.helpers import remove_part_of_string, append_to_string, replace_part_of_string
 """
 =====================================
-/report/wellformedness/v1 TESTS
+/report/wellformedness/v1 TESTS - (7 CASES TO BE TESTED)
 =====================================
 """
 # Wellformedness Testing that the report was generated properly and matches input data
@@ -161,142 +159,32 @@ def test_wrong_nesting_invalid():
     assert violation.message
     assert violation.suggestion
 
-
-# # Testing that a single rule fails when there is one attribute error in the invoice
-# def test_wellformed_single_violation():
+# def test_no_escape_for_special_char_invalid():
 #     data = VALID_INVOICE_TEXT
-
-#     # Invalidating the currency code by replacing AUD
-#     data = invalidate_invoice(data, 'attrib', 'cbc:Amount', 'currencyID', 'HELLO', 1)
-
+#     data = replace_part_of_string(data, 499, 500, "<")
 #     invoice = Invoice(name="My Invoice", source="text", data=data)
 
-#     wellformedness_evaluation = report_wellformedness_v1(invoice)
-#     wellformedness_evaluation = Evaluation(**wellformedness_evaluation)
+#     wellformed_evaluation = report_wellformedness_v1(invoice)
+#     wellformed_evaluation = Evaluation(**wellformed_evaluation)
 
-#     # We expect exactly 1 rule to fail due to the invalid currency code
-#     assert wellformedness_evaluation.num_rules_failed == 1
+#     # We expect exactly 1 rule to fail due to not escaping a special character
+#     assert wellformed_evaluation.num_rules_failed == 1
 
-#     # We expect exactly 1 violation due to the invalid currency code
-#     assert wellformedness_evaluation.num_violations == 1
+#     # We expect exactly 1 violation due to the special character
+#     assert wellformed_evaluation.num_violations == 1
 
 #     # Thus there should be exactly 1 violation in the violation list
-#     assert len(wellformedness_evaluation.violations) == 1
+#     assert len(wellformed_evaluation.violations) == 1
 
-#     code_violation = wellformedness_evaluation.violations[0]
-
-#     # From 'A-NZ_Invoice_Extension_v1.0.8.docx' file:
-#     # BR-CL-04 | [BR-CL-04]-Invoice currency code MUST be coded using ISO code list 4217 alpha-3 | Same | fatal
+#     violation = wellformed_evaluation.violations[0]
 
 #     # Check that the violation is for the correct rule and is flagged as fatal
-#     assert code_violation.rule_id == "BR-CL-03"
-#     assert code_violation.is_fatal == True
+#     assert violation.rule_id == "wellformedness" # need to find correct rule_id
+#     assert violation.is_fatal == True
 
-#     # Check that the violation has a non-empty message, test and suggestion
-#     assert code_violation.message
-#     assert code_violation.test
-#     assert code_violation.suggestion
-
-#     assert code_violation.location.type == "xpath"
-
-#     # Check that the location xpath is not empty
-#     assert code_violation.location.xpath
-
-# # Testing that a single rule fails when there is multiple attribute error in the invoice
-# def test_wellformed_multiple_violations_different_rules():
-#     data = VALID_INVOICE_TEXT
-
-#     # Invalidating 2 currency codes
-#     data = invalidate_invoice(data, 'attrib', 'cbc:Amount', 'currencyID', 'HELLO', 1)
-#     data = invalidate_invoice(data, 'attrib', 'cbc:Amount', 'currencyID', 'HELLO', 2)
-
-#     # Invalidating the 2 Country/IdentificationCode
-#     data = invalidate_invoice(data, 'content', 'cbc:IdentificationCode', '', 'HELLO', 1)
-#     data = invalidate_invoice(data, 'content', 'cbc:IdentificationCode', '', 'HELLO', 2)
-
-#     invoice = Invoice(name="My Invoice", source="text", data=data)
-
-#     wellformedness_evaluation = report_wellformedness_v1(invoice)
-#     wellformedness_evaluation = Evaluation(**wellformedness_evaluation)
-
-#     # We expect exactly 2 distinct rules to fail
-#     assert wellformedness_evaluation.num_rules_failed == 2
-
-#     # We expect exactly 2 violations for each invalid currency code and 2 violations for each invalid address
-#     assert wellformedness_evaluation.num_violations == 4
-
-#     # Thus there should be exactly 4 violations in the violation list
-#     assert len(wellformedness_evaluation.violations) == 4
-
-#     code_violation1 = wellformedness_evaluation.violations[0]
-#     code_violation2 = wellformedness_evaluation.violations[1]
-#     id_code_violation1 = wellformedness_evaluation.violations[2]
-#     id_code_violation2 = wellformedness_evaluation.violations[3]
-
-#     # Rule IDs should be the same for each currency code violation
-#     assert code_violation1.rule_id == code_violation2.rule_id == "BR-CL-14"
-
-#     # Rule IDs should be the same for each IdentificationCode violation
-#     assert id_code_violation1.rule_id == id_code_violation2.rule_id == "BR-CL-03"
-
-#     # Locations should be different for each violation
-#     assert code_violation1.location != code_violation2.location
-#     assert id_code_violation1.location != id_code_violation2.location
-
-def test_wellformedness_valid_version_number_error():
-    data = VALID_INVOICE_TEXT
-
-    invoice = Invoice(name="My Invoice", source="text", data=data)
-
-    wellformedness_evaluation = report_wellformedness_v1(invoice)
-    wellformedness_evaluation = Evaluation(**wellformedness_evaluation)
-    print(wellformedness_evaluation)
-    assert wellformedness_evaluation.aspect == "wellformedness"
-    assert data[15] == '1' or data[15] == '2'
-
-    # We expect exactly 1 rule to fail due to having no closing tag in the corresponding nest
-    assert wellformedness_evaluation.num_rules_failed == 0
-
-    # We expect exactly 1 violation due to the missing closing tag in the nest
-    assert wellformedness_evaluation.num_violations == 0
-
-    # Thus there should be exactly 1 violation in the violation list
-    assert len(wellformedness_evaluation.violations) == 0
-
-
-
-def test_wellformedness_invalid_version_number_error():
-    data = VALID_INVOICE_TEXT
-    data = replace_part_of_string(data, 15, 16, '5')
-
-    print(data[15:17])
-    invoice = Invoice(name="My Invoice", source="text", data=data)
-
-    wellformedness_evaluation = report_wellformedness_v1(invoice)
-    wellformedness_evaluation = Evaluation(**wellformedness_evaluation)
-    print(wellformedness_evaluation)
-    assert wellformedness_evaluation.aspect == "wellformedness"
-    assert data[15] == '5'
-
-    # We expect exactly 1 rule to fail due to having no closing tag in the corresponding nest
-    assert wellformedness_evaluation.num_rules_failed == 1
-
-    # We expect exactly 1 violation due to the missing closing tag in the nest
-    assert wellformedness_evaluation.num_violations == 1
-
-    # Thus there should be exactly 1 violation in the violation list
-    assert len(wellformedness_evaluation.violations) == 1
-
-    violation = wellformedness_evaluation.violations[0]
-
-    # Check that the violation is for the correct rule and is flagged as fatal
-    assert violation.rule_id == "wellformedness" # need to find correct rule_id
-    assert violation.is_fatal == True
-
-    # Check that the violation has a non-empty message and suggestion
-    assert violation.message
-    assert violation.suggestion
-
+#     # Check that the violation has a non-empty message and suggestion
+#     assert violation.message
+#     assert violation.suggestion
 
 # def test_escape_special_char_valid():
 #     data = VALID_INVOICE_TEXT
@@ -314,32 +202,3 @@ def test_wellformedness_invalid_version_number_error():
 
 #     # Thus there should've be any violation
 #     assert len(wellformed_evaluation.violations) == 0
-
-
-def test_no_escape_for_special_char_invalid():
-    data = VALID_INVOICE_TEXT
-    data = replace_part_of_string(data, 499, 500, "<")
-    invoice = Invoice(name="My Invoice", source="text", data=data)
-
-    wellformed_evaluation = report_wellformedness_v1(invoice)
-    wellformed_evaluation = Evaluation(**wellformed_evaluation)
-
-    # We expect exactly 1 rule to fail due to not escaping a special character
-    assert wellformed_evaluation.num_rules_failed == 1
-
-    # We expect exactly 1 violation due to the special character
-    assert wellformed_evaluation.num_violations == 1
-
-    # Thus there should be exactly 1 violation in the violation list
-    assert len(wellformed_evaluation.violations) == 1
-
-    violation = wellformed_evaluation.violations[0]
-
-    # Check that the violation is for the correct rule and is flagged as fatal
-    assert violation.rule_id == "wellformedness" # need to find correct rule_id
-    assert violation.is_fatal == True
-
-    # Check that the violation has a non-empty message and suggestion
-    assert violation.message
-    assert violation.suggestion
-
