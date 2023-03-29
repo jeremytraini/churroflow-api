43f0f8a5d737687663ecae1a93340f8b893f97e9
dzl-i <denzel.iskandar@gmail.com>
2023-03-30 00:27:57 +1100

    Added testing for errors in invoice_upload

--- tests/invoice/upload_file_test.py
+++ tests/invoice/upload_file_test.py
@@ -13,3 +13,7 @@ def test_upload_file_valid_invoice():
     response = invoice_upload_file_v1("tests/example_files/AUInvoice.xml")
     
     assert response['report_id'] >= 0
+
+def test_upload_file_invalid_invoice_format():
+    
+    assert invoice_upload_file_v1("tests/example_files/AUInvoice.txt")['detail'] == "Invoice file type is not XML"
