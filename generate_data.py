02e2b16e6d524bb0b304dc52e878845ab714d22d
Jeremy Traini <z5362885@ad.unsw.edu.au>
2023-04-13 12:53:58 +1000

    Further fixes to data generation script

--- generate_data.py
+++ generate_data.py
@@ -1,10 +1,13 @@
 from faker import Faker
 import random
 import datetime
-from src.database import db
+from src.database import Invoices, LineItems, db
 
 # Instantiate a Faker object
 fake = Faker()
+
+NUM_INVOICES = 50
+NUM_LINE_ITEMS = 200
 
 def random_lat():
     min_lat, max_lat = -34.05, -33.568
@@ -14,7 +17,12 @@ def random_lon():
     min_lon, max_lon = 150.52, 151.34
     return round(random.uniform(min_lon, max_lon), 6)
 
-supplier = ("Churros Pty Ltd", "12345678901", random_lat(), random_lon())
+supplier = ("Churros Pty Ltd", "12345678901")
+supplier_warehouses = [
+    (random_lat(), random_lon()),
+    (random_lat(), random_lon()),
+    (random_lat(), random_lon()),
+]
 
 fake_customers = [(fake.company(),
                    fake.random_number(digits=9),
@@ -27,7 +35,7 @@ fake_customers = [(fake.company(),
 
 # Generate fake data for the invoices table
 invoices = []
-for i in range(1, 101):
+for i in range(1, NUM_INVOICES + 1):
     customer = random.choice(fake_customers)
     
     issue_date = fake.date_time_between(start_date="-2y", end_date="now")
@@ -38,8 +46,9 @@ for i in range(1, 101):
     
     delivery_date = fake.date_time_between(start_date=start_date, end_date=start_date + datetime.timedelta(days=10))
     
+    supplier_warehouse = random.choice(supplier_warehouses)
+    
     invoices.append((
-        i,
         "invoice{}.xml".format(i),
         1,
         fake.date_time_between(start_date="-1d", end_date="now").strftime('%Y-%m-%d'),
@@ -56,8 +65,8 @@ for i in range(1, 101):
         end_date.strftime('%Y-%m-%d'),
         supplier[0],
         supplier[1],
-        supplier[2],
-        supplier[3],
+        supplier_warehouse[0],
+        supplier_warehouse[1],
         customer[0],
         customer[1],
         delivery_date.strftime('%Y-%m-%d'),
@@ -87,39 +96,33 @@ possible_items = [
 
 # Generate fake data for the lineitems table
 lineitems = []
-for i in range(1, 1001):
+for i in range(1, NUM_LINE_ITEMS + 1):
     item = random.choice(possible_items)
     quantity = random.randint(1, 10)
     
     lineitems.append((
-        i,
-        random.randint(1, 100),
+        random.randint(1, NUM_INVOICES),
         item[0],
         quantity,
         item[1],
         round(quantity * item[1], 2)
     ))
 
-    # Calculate total_price
-    # lineitems[-1] = lineitems[-1][:5] + (lineitems[-1][3] * lineitems[-1][4],)
-
-# Output the fake data
-# print("INSERT INTO invoices (id, name, owner_id, date_last_modified, date_added, num_warnings, num_errors, is_valid, text_content, invoice_title, issue_date, due_date, order_id, invoice_start_date, invoice_end_date, supplier_name, supplier_abn, supplier_latitude, supplier_longitude, customer_name, customer_abn, delivery_date, delivery_latitude, delivery_longitude, customer_contact_name, customer_contact_email, customer_contact_phone, total_amount) VALUES")
-# print(",\n".join(str(invoice) for invoice in invoices))
-
-# print("\n\nINSERT INTO lineitems (id, invoice_id, description, quantity, unit_price, total_price) VALUES")
-# print(",\n".join(str(lineitem) for lineitem in lineitems))
-
 # save the fake data to a csv
-with open('invoices.csv', 'w') as f:
-    f.write("id,name,owner_id,date_last_modified,date_added,num_warnings,num_errors,is_valid,text_content,invoice_title,issue_date,due_date,order_id,invoice_start_date,invoice_end_date,supplier_name,supplier_abn,supplier_latitude,supplier_longitude,customer_name,customer_abn,delivery_date,delivery_latitude,delivery_longitude,customer_contact_name,customer_contact_email,customer_contact_phone,total_amount")
-    for invoice in invoices:
-        f.write(f"\n{','.join(str(x) for x in invoice)}")
+# with open('invoices.csv', 'w') as f:
+#     f.write("id,name,owner_id,date_last_modified,date_added,num_warnings,num_errors,is_valid,text_content,invoice_title,issue_date,due_date,order_id,invoice_start_date,invoice_end_date,supplier_name,supplier_abn,supplier_latitude,supplier_longitude,customer_name,customer_abn,delivery_date,delivery_latitude,delivery_longitude,customer_contact_name,customer_contact_email,customer_contact_phone,total_amount")
+#     for invoice in invoices:
+#         f.write(f"\n{','.join(str(x) for x in invoice)}")
     
-with open('lineitems.csv', 'w') as f:
-    f.write("id,invoice_id,description,quantity,unit_price,total_price")
-    for lineitem in lineitems:
-        f.write(f"\n{','.join(str(x) for x in lineitem)}")
+# with open('lineitems.csv', 'w') as f:
+#     f.write("id,invoice_id,description,quantity,unit_price,total_price")
+#     for lineitem in lineitems:
+#         f.write(f"\n{','.join(str(x) for x in lineitem)}")
 
+# Clear Invoices and LineItems tables
+db.drop_tables([Invoices, LineItems])
+db.create_tables([Invoices, LineItems])
 
-
+# Add the fake data to the database
+Invoices.insert_many(invoices).execute()
+LineItems.insert_many(lineitems).execute()
